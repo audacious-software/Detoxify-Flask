3bf4edf24e2582d9ed72f5527a4062506f95016e
Chris J. Karr <chris@audacious-software.com>
2023-04-10 16:50:38 +0000

    Code cleanup and PyLint configuration

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+# pylint: disable=line-too-long
+
 import json
+import threading
+import traceback
+
+from flask import Flask, current_app, request, Response
 
 from detoxify import Detoxify
-
-from flask import Flask, current_app, request
 
 app = Flask(__name__)
 
@@ -14,10 +18,18 @@ DETOXIFY_MODELS = (
 
 DETOXIFY_MODELS_ACTIVE = {}
 
+LAST_TEXT = None
+LAST_SCORES = None
+
+WAIT_LOCK = threading.Semaphore()
+
 @app.route('/', methods=['POST', 'GET'])
 def score():
+    global LAST_TEXT # pylint: disable=global-statement
+    global LAST_SCORES # pylint: disable=global-statement
+
     to_score = request.form.get('s')
-    
+
     if to_score is None:
         to_score = request.args.get('s')
 
@@ -27,20 +39,43 @@ def score():
     scores = {
         'to_score': to_score
     }
-    
-    for model in DETOXIFY_MODELS:
-        model_instance = DETOXIFY_MODELS_ACTIVE.get(model, None)
-        
-        if model_instance is None:
-            model_instance = Detoxify(model)
-            
-            DETOXIFY_MODELS_ACTIVE[model] = model_instance
 
-        model_scores = model_instance.predict(to_score)
+    errored = False
 
-        for key in model_scores:
-            model_scores[key] = float(model_scores[key])
+    try:
+        WAIT_LOCK.acquire() # pylint: disable=consider-using-with
 
-        scores[model] = model_scores
-    
+        if to_score == LAST_TEXT:
+            scores = LAST_SCORES
+        else:
+            for model in DETOXIFY_MODELS:
+                model_instance = DETOXIFY_MODELS_ACTIVE.get(model, None)
+
+                if model_instance is None:
+                    print('[Detoxify] Creating model for "%s"...' % model)
+                    model_instance = Detoxify(model)
+
+                    DETOXIFY_MODELS_ACTIVE[model] = model_instance
+
+                    print('[Detoxify] Model "%s" created and persisted to memory.' % model)
+
+                model_scores = model_instance.predict(to_score)
+
+                for key in model_scores:
+                    model_scores[key] = float(model_scores[key])
+
+                scores[model] = model_scores
+
+            LAST_TEXT = to_score
+            LAST_SCORES = scores
+    except: # pylint: disable=bare-except
+        traceback.print_exc()
+
+        errored = True
+    finally:
+        WAIT_LOCK.release()
+
+    if errored:
+        return Response('Error encountered scoring "%s". Check logs for full details.' % to_score, status=500)
+
     return current_app.response_class(json.dumps(scores, indent=2), mimetype='application/json') # jsonify(scores)
